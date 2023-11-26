c021139ad81663892462f0865a0573fb7bef3cc7
Vladimir Voblikov <vladimir.voblikov.774@gmail.com>
2023-11-27 03:39:39 +0500

    task 445: adding rest assured: second try

---
+++ src/main/java/org/ot5usk/models/add_new_author/AddNewAuthorRequest.java
@@ -0,0 +1,19 @@
+package org.ot5usk.models.add_new_author;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AddNewAuthorRequest {
+
+    @NonNull
+    private String firstName;
+
+    @NonNull
+    private String familyName;
+    private String secondName;
+}
